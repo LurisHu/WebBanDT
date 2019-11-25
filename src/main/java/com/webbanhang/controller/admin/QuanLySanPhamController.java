b27c4798b58461a434a2a5dfb36eaef727ffc85f
Ho Viet Hai <hoviethai1999@gmail.com>
2019-11-25 21:59:05 +0700

    fix upload image sanpham function

--- src/main/java/com/webbanhang/controller/admin/QuanLySanPhamController.java
+++ src/main/java/com/webbanhang/controller/admin/QuanLySanPhamController.java
@@ -22,40 +22,38 @@ import com.webbanhang.dao.SanPhamDAO;
 import com.webbanhang.entity.SanPham;
 import com.webbanhang.service.UploadService;
 
-
-
 @Controller
 public class QuanLySanPhamController {
 	@Autowired
 	SanPhamDAO dao;
-	
+
 	@Autowired
 	NhaCungCapDAO nccdao;
-	
+
 	@Autowired
 	DanhMucDAO dmdao;
-	
+
 	@Autowired
 	UploadService uploadService;
-	
+
 	@RequestMapping("admin/sanpham/index/{pageNo}")
 	public String index(Model model, @PathVariable("pageNo") int pageNo) {
-		if(pageNo >= dao.getPageCountProducts()) {
+		if (pageNo >= dao.getPageCountProducts()) {
 			pageNo = 0;
-		}else if(pageNo < 0) {
-			pageNo = dao.getPageCountProducts()-1;
+		} else if (pageNo < 0) {
+			pageNo = dao.getPageCountProducts() - 1;
 		}
 		model.addAttribute("form", new SanPham());
 		model.addAttribute("products", dao.findPageProduct(pageNo));
 		model.addAttribute("nccs", nccdao.findAll());
 		model.addAttribute("danhmuc", dmdao.findAll());
 		model.addAttribute("pageNo", pageNo);
-		model.addAttribute("lastPageNo", dao.getPageCountPhone()-1);
+		model.addAttribute("lastPageNo", dao.getPageCountPhone() - 1);
 		return "admin/sanpham/index";
 	}
-	
+
 	@RequestMapping("admin/sanpham/edit/{id}")
-	public String edit(Model model, @PathVariable("id")Integer id) {
+	public String edit(Model model, @PathVariable("id") Integer id) {
 		int pageNo = 0;
 		SanPham entity = dao.findById(id);
 		model.addAttribute("form", entity);
@@ -64,9 +62,9 @@ public class QuanLySanPhamController {
 		model.addAttribute("danhmuc", dmdao.findAll());
 		return "admin/sanpham/index";
 	}
-	
+
 	@RequestMapping("admin/sanpham/create")
-	public String create(Model model, SanPham entity,@RequestParam("up_photo") MultipartFile file) {
+	public String create(Model model, SanPham entity, @RequestParam("up_photo") MultipartFile file) {
 		entity.setImage(uploadService.uploadImage(file));
 		dao.create(entity);
 		model.addAttribute("form", entity);
@@ -75,10 +73,15 @@ public class QuanLySanPhamController {
 		model.addAttribute("danhmuc", dmdao.findAll());
 		return "redirect:/admin/sanpham/index/0";
 	}
-	
+
 	@RequestMapping("admin/sanpham/update")
-	public String update(Model model, SanPham entity,@RequestParam("up_photo") MultipartFile file) {
-		entity.setImage(uploadService.uploadImage(file));
+	public String update(Model model, SanPham entity, @RequestParam("up_photo") MultipartFile file) {
+		
+		if (file.isEmpty() && !(dao.findById(entity.getMaSP()).getImage().equals("default.png"))) {
+			entity.setImage(dao.findById(entity.getMaSP()).getImage());
+		}else {
+			entity.setImage(uploadService.uploadImage(file));
+		}
 		dao.update(entity);
 		model.addAttribute("form", entity);
 		model.addAttribute("products", dao.findAll());
@@ -86,7 +89,7 @@ public class QuanLySanPhamController {
 		model.addAttribute("danhmuc", dmdao.findAll());
 		return "redirect:/admin/sanpham/index/0";
 	}
-	
+
 	@RequestMapping("admin/sanpham/delete")
 	public String delete(Model model, SanPham entity) {
 		dao.delete(entity.getMaSP());
